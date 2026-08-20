6610e52af8585d75f935040aef0968291ca934af
Chandra Shekhar <chandrashekhar2001K@gmail.com>
2026-08-20 16:58:11 +0530

    added contentprotection file

--- src/Website/index.jsx
+++ src/Website/index.jsx
@@ -1,11 +1,14 @@
 import "./website.css";
 import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
+import ContentProtection from "./components/ContentProtection";
 
 /** Exact BizOne landing page from Downloads/BizOne_landing_page */
 export default function WebsiteLanding() {
   return (
     <div className="bizone-landing">
+      <ContentProtection />
+
       <Navbar />
       <LandingPage />
     </div>
